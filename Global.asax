c31dd39e2b5a2688c59384c017aaadc5fa94c9dd
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2017-05-25 02:44:04 -0500

    Initial commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2017_05_24ULSAVentas.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_2017_05_24ULSAVentas.Startup))]
+namespace _2017_05_24ULSAVentas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
