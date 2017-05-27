d1414e1fdb70679c38c84519c80bf2771ff5ec85
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2017-05-27 01:04:05 -0500

    API de Maps casi completa

---
+++ Controllers/PruebasController.cs
@@ -0,0 +1,34 @@
+﻿using _2017_05_24ULSAVentas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2017_05_24ULSAVentas.Controllers
+{
+    public class PruebasController : Controller
+    {
+        private static ULSAVentasDataContext _db = new ULSAVentasDataContext();
+        private static ULSAVentasDataContext db
+        {
+            get
+            {
+                return _db;
+            }
+        }
+
+
+        // GET: Pruebas
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(db.Direccion.ToList());
+        }
+
+        public ActionResult MapaParcial()
+        {
+            return View();
+        }
+    }
+}
