f690161b6c88229b7a464141800c042a1347312e
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2017-06-15 04:27:44 -0500

    Comienzo del módulo de perfil.
    Apenas comenzamos. Pero hasta aquí lo dejo para subirlo al Servidor.

---
+++ Controllers/PerfilController.cs
@@ -0,0 +1,53 @@
+﻿using _2017_05_24ULSAVentas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2017_05_24ULSAVentas.Controllers
+{
+    public class PerfilController : Controller
+    {
+        private static ULSAVentasDataContext db
+        {
+            get
+            {
+                return ContextoEstatico.db;
+            }
+        }
+
+        // GET: Perfil
+        public ActionResult Index(int idUsuario)
+        {
+            Usuario u = null;
+
+            try
+            {
+                u = db.Usuario.First();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(u);
+        }
+
+        public ActionResult RenderizarPerfil(int idUsuario)
+        {
+            Usuario u = null;
+
+            try
+            {
+                u = db.Usuario.First();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return PartialView(u);
+        }
+    }
+}
